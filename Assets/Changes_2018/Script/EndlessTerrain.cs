48940b9e43d0fef27611d9ef9129237759bd5be7
andreabefuen <al341488@uji.es>
2018-10-29 18:00:04 +0100

    Tutorial 7 hasta min 7

---
+++ Assets/Changes_2018/Script/EndlessTerrain.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EndlessTerrain : MonoBehaviour {
+
+    public const float maxViewDst = 300;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunksVisibleInViewDst;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+
+    void Start()
+    {
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDst =Mathf.RoundToInt( maxViewDst / chunkSize);
+    }
+    
+    void UpdateVisibleChunks()
+    {
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for(int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++)
+        {
+            for (int xOffset = 0; xOffset <= chunksVisibleInViewDst; xOffset++)
+            {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                if(terrainChunkDictionary.ContainsKey(viewedChunkCoord))
+                {
+
+                }
+                else
+                {
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk());
+                }
+            }
+        }
+
+    }
+
+    public class TerrainChunk
+    {
+
+    }
+
+}
