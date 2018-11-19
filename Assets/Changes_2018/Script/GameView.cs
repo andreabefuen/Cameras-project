aeb6351687de08434eedce99448c2afad37ca072
andreabefuen <al341488@uji.es>
2018-11-19 16:20:16 +0100

    Falta la cámara que no se salga

--- Assets/Changes_2018/Script/GameView.cs
+++ Assets/Changes_2018/Script/GameView.cs
@@ -8,17 +8,30 @@ public class GameView : MonoBehaviour {
     public Text timerText;
     public Text readyText;
 
+    public GameObject initialPanel;
+
     private float timer;
 
 	// Use this for initialization
 	void Start () {
+       
         readyText.enabled = false;
-	}
+        initialPanel.SetActive(true);
+        
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
         timerText.text = "TIMER: " + (int) timer;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && initialPanel.activeSelf == true)
+        {
+            //Time.timeScale = 1;
+            initialPanel.SetActive(false);
+        }
 
         if(timer >= 14 && timer <17)
         {
@@ -40,7 +53,10 @@ public class GameView : MonoBehaviour {
 
         if (timer > 60f)
         {
+            
             Debug.Log("Terminar juego");
+            int aux = Application.loadedLevel + 1;
+            Application.LoadLevel(aux);
         }
 	}
 
